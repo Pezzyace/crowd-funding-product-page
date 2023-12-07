a51c1a738d5d544ffde06dbd769d3f88a9a45e6a
Akaehomen peace <peacedavid049@gmail.com>
2023-12-07 04:20:00 +0100

    commit

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
 const closeSelectionModalBtn = document.getElementById("closeSelectionModalBtn");
 const selectRewardBtn = document.getElementById("selectReward-btn");
 const selectionModal = document.getElementById("selectionModal");
-const contineBtn = document.getElementById("contineBtn");
-const radio = document.querySelectorAll("radio");
-const pledgeContainer = document.getElementById("pledgeContainer");
-const article = document.getElementById("article");
+const continueBtn = document.getElementById("continueBtn");
 
 
+
+function showInfo() {
+  const bambooOptionInfo = document.getElementById('bambooOptionInfo');
+  const blackOptionInfo = document.getElementById('bambooOptionInfo');
+
+
+  const selectedOption = document.querySelector('input[name="selected"]:checked').value;
+
+  if (selectedOption === 'bambooOption') {
+    bambooOptionInfo.style.display = 'block';
+  } else if (selectedOption === 'blackOption') {
+    blackOptionInfo.style.display = 'block';
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let totalPledged = 89914; // Default amount already pledged
+let pledgeCount = 56; // Counter for the number of people pledged
+const maxAmount = 100000;
+
+
+
+
+
+function makePledge() {
+
+}
+
+function updateProgress() {
+  const progressBar = document.getElementById('progressBar');
+  const pledgeCountElement = document.getElementById('pledgeCount');
+  
+  const percentage = (totalPledged / maxAmount) * 100;
+  progressBar.style.width = percentage + '%';
+  pledgeCountElement.textContent = pledgeCount;
+}
+
+
+
+//Button Reactions
 selectRewardBtn.addEventListener("click", () => {
   selectionModal.style.display = "flex";
 })
 closeSelectionModalBtn.addEventListener("click", () => {
   selectionModal.style.display = "none";
-})
-radio.addEventListener("click", () => {
-  article.style.display = "flex";
 })
 
